4c01fb4ef554819242118617c7de54fe8926e279
himanshusingh412 <himanshux412@gmail.com>
2025-11-10 14:38:31 +0530

    test1

---
+++ st1exam.html.h
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+<h1 align="center">RESUME</h1>
+</title>
+</head>
+<body>
+<!--Heading-->
+<h1 align center="center">EXPERIMENT 1</h1>
+<p align="center">WEB DESIGNING</p>
+<hr>
+<!-- Introduction -->
+<h2>Introduction</h2>
+<p>
+Hello my name is <b>Himanshu Singh</b> and I have completed my schooling from <i>MPVM Ganga gurukulam</i> and now persuing my college in <u>ABESECCollege</u>
+</p>
+<!-- Ordered List -->
+ <h2>Knowledge</h2>
+ <ol>
+<li>Python</li>
+<li>c++</li>
+<li>Java</li>
+ </ol>
+<!-- Unordered List -->
+<h2>Hobbey</h2>
+<ul>
+<li>Singing</li>
+ <li>Gamimg</li>
+ <li>Coding</li>
+</ul>
+<!--Descriptive list-->
+<dl> Course Development</dl>
+<dt>What you have  study</dt>
+<dd>We have learned c++,java,python,javascript</dd>
+</dl>
+<!--Nested list-->
+<h2>Certificate</h2>
+<ol>
+<li>Google</li>
+<ul>
+<li>Gesawk</li>
+<li>Google summer code</li>
+</ul>
+</ol>
+<!--Mixed Type-->
+<h2>Intership</h2>
+<ul>
+<li>Microsogt</li>
+<ol>
+<li>Deloitte</li>
+</ol>
+</ul>
+<!--Footer-->
+<hr>
+<p align="center">&copy</p>
+</body>
+</html>
+
